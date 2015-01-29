30ebee009f3fdeccab91e51d9c1fe71a784133aa
mwoods <mwoods@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-01-29 12:32:08 +0000

    load.R uses common method on Windows and other platforms,   although the Windows version assumes that udunits2 is linked.
    git-svn-id: svn+ssh://r-forge.r-project.org/svnroot/rnetcdf/pkg@44 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/windows/load.R
+++ R/windows/load.R
@@ -1,15 +1,26 @@
-".onLoad" <- function(lib, pkg) {
-    # Initialise udunits library and allow it to find a units database.
-    # First, environment variables are checked:
-    #   udunits2 uses UDUNITS2_XML_PATH,
-    #   udunits uses UDUNITS_PATH.
-    # Then the library will try a default path set at compile time.
-#    result <- try(utinit.nc(""), silent=TRUE)
+# Assume that RNetCDF is linked with udunits2 in Makevars.win.
 
-#    if (inherits(result,"try-error")) {
-        # Try udunits2 database that ships with RNetCDF:
-        units2 <- system.file("udunits", "udunits2.xml", 
-                    package=pkg, lib.loc=lib, mustWork=TRUE)
-        utinit.nc(units2)
-#    }
+".onLoad" <- function(lib, pkg) {
+
+    # Initialise udunits library and allow it to find a units database
+    # in the UDUNITS2_XML_PATH environment variable or a built-in default path:
+    result <- try(utinit.nc(""), silent=TRUE)
+
+    # If library failed to initialise,
+    # try to use the units database shipped with RNetCDF.
+    if (inherits(result, "try-error")) {
+        datafile <- system.file("udunits", "udunits2.xml", 
+           package=pkg, lib.loc=lib, mustWork=TRUE)
+
+        # Set the environment so that nested database files can be found:
+	Sys.setenv(UDUNITS2_XML_PATH=datafile)
+
+        result <- try(utinit.nc(""), silent=TRUE)
+
+        if (inherits(result, "try-error")) {
+	    packageStartupMessage("RNetCDF: udunits2 library failed to initialise")
+        } else {
+	    packageStartupMessage("RNetCDF: udunits2.xml loaded from RNetCDF package")
+        }
+    }
 }
