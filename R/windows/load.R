9e4069f6e0e45333c7ddadf10c1b2339d18db34e
mwoods <mwoods@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-01-31 06:30:52 +0000

    load.R reports problems if udunits(2) fails with database specified by environment variable.
    git-svn-id: svn+ssh://r-forge.r-project.org/svnroot/rnetcdf/pkg@48 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/windows/load.R
+++ R/windows/load.R
@@ -2,25 +2,37 @@
 
 ".onLoad" <- function(lib, pkg) {
 
-    # Initialise udunits library and allow it to find a units database
-    # in the UDUNITS2_XML_PATH environment variable or a built-in default path:
+    # Initialise udunits2 library and allow it to find a units database in
+    # environment variable UDUNITS2_XML_PATH or a built-in default path:
     result <- try(utinit.nc(""), silent=TRUE)
 
-    # If library failed to initialise,
-    # try to use the units database shipped with RNetCDF.
+    # If library failed to initialise:
     if (inherits(result, "try-error")) {
-        datafile <- system.file("udunits", "udunits2.xml", 
-           package=pkg, lib.loc=lib, mustWork=TRUE)
 
-        # Set the environment so that nested database files can be found:
-	Sys.setenv(UDUNITS2_XML_PATH=datafile)
+        # Stop if environment variable UDUNITS2_XML_PATH was set:
+        if (nchar(Sys.getenv("UDUNITS2_XML_PATH")) > 0) {
+	    packageStartupMessage(
+"RNetCDF: udunits2 failed to initialise with units database
+           in file specified by environment variable UDUNITS2_XML_PATH")
+            return()
+        }
 
-        result <- try(utinit.nc(""), silent=TRUE)
+        # Try the units database that ships with RNetCDF.
+        # Set UDUNITS2_XML_PATH so that nested database files can be found.
+	result <- try({
+            datafile <- system.file("udunits", "udunits2.xml", package=pkg, lib.loc=lib)
+            Sys.setenv(UDUNITS2_XML_PATH=datafile)
+            utinit.nc("")
+            }, silent=TRUE)
 
         if (inherits(result, "try-error")) {
-	    packageStartupMessage("RNetCDF: udunits2 library failed to initialise")
+	    packageStartupMessage(
+"RNetCDF: udunits2 failed to initialise with in-built units database.
+           Please specify a file in environment variable UDUNITS2_XML_PATH.")
         } else {
-	    packageStartupMessage("RNetCDF: udunits2.xml loaded from RNetCDF package")
+            # Return silently on success, because user probably doesn't care that
+            # the internal database was used unless they set UDUNITS2_XML_PATH.
         }
     }
 }
+
