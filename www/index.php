e66f322d2d382db3446c4cc536da91771b734501
mwoods <mwoods@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-02-01 06:06:44 +0000

    index.php: added link to R Journal paper for description about RNetCDF.   More content will need to be added later ...
    git-svn-id: svn://r-forge.r-project.org/svnroot/rnetcdf@54 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -43,7 +43,7 @@ echo $contents; } ?>
 
 <!-- end of project description -->
 
-<p> No content added. </p>
+<p> An introduction to the RNetCDF package is given in an <a href="http://journal.r-project.org/archive/2013-2/michna-woods.pdf"><strong>R Journal paper</strong></a> by the package authors. </p>
 
 <p> The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
 
