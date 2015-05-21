167ce3865be9023bb807cfdbdcaee3876e46dd17
mwoods <mwoods@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-05-21 21:12:21 +0000

    1.7-4: fix --with-netcdf-lib and --with-udunits-lib options
    git-svn-id: svn://r-forge.r-project.org/svnroot/rnetcdf@64 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/RNetCDF.R
+++ pkg/R/RNetCDF.R
@@ -2,7 +2,7 @@
 #										#
 #  Name:       RNetCDF.R							#
 #										#
-#  Version:    1.7-3								#
+#  Version:    1.7-4								#
 #										#
 #  Purpose:    NetCDF interface for R.						#
 #										#
--- pkg/src/RNetCDF.c
+++ pkg/src/RNetCDF.c
@@ -2,7 +2,7 @@
  *									       *
  *  Name:       RNetCDF.c						       *
  *									       *
- *  Version:    1.7-3							       *
+ *  Version:    1.7-4							       *
  *									       *
  *  Purpose:    NetCDF interface for R.					       *
  *									       *
