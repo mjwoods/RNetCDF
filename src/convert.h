14fb9ffd142d296552c06266ff20d44a4d45859b
Milton Woods <miltonjwoods@gmail.com>
2018-07-21 17:13:24 +1000

    Update header for generic conversions

--- src/convert.h
+++ src/convert.h
@@ -44,52 +44,29 @@
 int isInt64(SEXP rv);
 
 
-/* TODO: roll these into R_nc_r2c and R_nc_c2r */
-
-void
-R_nc_strsxp_char (SEXP rstr, char *carr, size_t imin, size_t cnt,
-                  size_t strlen);
-
-void
-R_nc_char_strsxp (char *carr, SEXP rstr,
-                  size_t len, size_t imin, size_t cnt);
-
-
-void
-R_nc_strsxp_str (SEXP rstr, const char **cstr, size_t imin, size_t cnt);
-
-
-void
-R_nc_str_strsxp (char **cstr, SEXP rstr, size_t imin, size_t cnt);
-
-
-/* Convert an R vector to a netcdf external type.
-   Argument rv contains at least cnt values from index imin.
-   Argument cv is a pointer to a C vector.
-     If cv is NULL on entry, memory is allocated by R_alloc.
+/* Convert an R vector to a netcdf external type (xtype).
+   Memory for the results is allocated by R_alloc (freed by R).
+   The number and lengths of netcdf dimensions are ndim and xdim (C-order).
    An error is raised for out-of-range values.
    Missing and NaN values are replaced by a fill value.
    Packing is performed if either scale or add are not NULL.
-   Example: R_nc_r2c (rv, cv, cnt, &fill, &scale, &add);
  */
 void *
-R_nc_r2c (SEXP rv, void *cv, size_t imin, size_t cnt, nc_type xtype,
+R_nc_r2c (SEXP rv, int ncid, nc_type xtype, int ndim, size_t *xdim,
           void *fill, double *scale, double *add);
 
 
-/* Convert a vector of netcdf external type to R.
-   Argument cv is assumed to contain cnt values of the external type.
-   If fitnum is true (non-zero), the rv is the smallest compatible R type,
+/* Convert an array of netcdf external type (xtype) to R.
+   Memory for the results is allocated by allocArray.
+   The number and lengths of netcdf dimensions are ndim and xdim (C-order).
+   If fitnum is true (non-zero), the rv is the smallest R numeric type,
      otherwise rv is double precision.
-   Elements are set to missing if they are NaN,
-     equal to the fill value or outside the valid range (if not NULL).
+   Elements are set to missing if they equal the fill value.
    Unpacking is performed if either scale or add are not NULL.
-   Example: R_nc_c2r (cv, rv, imin, cnt, xtype, 1,
-                      &fill, &min, &max, &scale, &add);
  */
 SEXP
-R_nc_c2r (void *cv, size_t imin, size_t cnt, nc_type xtype, int fitnum,
-          void *fill, void *min, void *max, double *scale, double *add);
+R_nc_c2r (void *cv, int ncid, nc_type xtype, int ndim, size_t *xdim, int fitnum,
+          void *fill, double *scale, double *add);
 
 
 /* Reverse a vector in-place.
