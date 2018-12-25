4e4fb898b774a29e88b26011aa43f324ab5911ba
Milton Woods <miltonjwoods@gmail.com>
2018-12-25 15:31:25 +1100

    Extra argument checks in dataset.c

--- R/RNetCDF.R
+++ R/RNetCDF.R
@@ -203,7 +203,6 @@ create.nc <- function(filename, clobber = TRUE, share = FALSE, prefill = TRUE,
   stopifnot(is.logical(prefill))
   stopifnot(is.character(format))
   stopifnot(is.logical(large))
-  stopifnot(nchar(filename) > 0L)
   
   #-- C function call --------------------------------------------------------
   nc <- .Call(R_nc_create, filename, clobber, share, prefill, format)
@@ -293,7 +292,6 @@ open.nc <- function(con, write = FALSE, share = FALSE, prefill = TRUE, ...) {
   stopifnot(is.logical(write))
   stopifnot(is.logical(share))
   stopifnot(is.logical(prefill))
-  stopifnot(nchar(con) > 0L)
   
   #-- C function call --------------------------------------------------------
   nc <- .Call(R_nc_open, con, write, share, prefill)
