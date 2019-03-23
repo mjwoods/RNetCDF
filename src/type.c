3a50a10ab1301f500953cea0f177c92974426ca4
Milton Woods <miltonjwoods@gmail.com>
2019-03-23 17:52:10 +1100

    Avoid pointer arithmetic with void type

--- src/type.c
+++ src/type.c
@@ -340,7 +340,7 @@ R_nc_inq_type (SEXP nc, SEXP type, SEXP fields)
   char fieldname[NC_MAX_NAME + 1], subname[NC_MAX_NAME + 1];
   size_t size, nfields, offset;
   int ii, imax, ndims;
-  void *cval;
+  char *cval;
   SEXP result=R_NilValue, resultnames=R_NilValue;
   SEXP fieldnames, values, offsets, subnames, dimsize, dimsizes;
   R_nc_buf io;
