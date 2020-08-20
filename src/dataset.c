33e9fa4c75fe08dcb5d1be33e545edac8c30b547
Milton Woods <miltonjwoods@gmail.com>
2020-08-20 22:27:32 +1000

    Avoid build failure if data64 is not supported by NetCDF library

--- src/dataset.c
+++ src/dataset.c
@@ -166,7 +166,11 @@ R_nc_create (SEXP filename, SEXP clobber, SEXP share, SEXP prefill,
   } else if (R_nc_strcmp(format, "offset64")) {
     cmode = cmode | NC_64BIT_OFFSET;
   } else if (R_nc_strcmp(format, "data64")) {
+#ifdef NC_64BIT_DATA
     cmode = cmode | NC_64BIT_DATA;
+#else
+    error("NetCDF library does not support data64 format");
+#endif
   }
 
   /*-- Create the file --------------------------------------------------------*/
