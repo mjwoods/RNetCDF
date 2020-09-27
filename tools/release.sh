ff466b3b603e20636ccd4b310d603754d7b919da
Milton Woods <miltonjwoods@gmail.com>
2020-09-27 17:46:35 +1000

    Add release script (#85)
    * Provide release.sh to prepare for package releases
    
    * Compare commit timestamps in release.sh
    
    * Preserve execute permission when preparing release
    
    * Warn about uncommitted files when creating release
    
    * Run git commands inside package directory in release.sh
    
    * Avoid changing release strings in NEWS with release.sh
    
    * Update date in DESCRIPTION with release.sh
    
    * Handle BSD or GNU syntax for date command
    
    * Use git author date when comparing timestamps on files
    
    * Hide output from test of date command

---
+++ tools/release.sh
@@ -0,0 +1,85 @@
+#!/bin/sh
+# Prepare for new RNetCDF release
+
+set -e
+set -u
+
+# Check inputs:
+if [[ $# -ne 1 ]]; then
+  echo "Usage: $0 x.y-z" >&2
+  exit 1
+fi
+
+newver="$1"
+if ! echo "$newver" | grep -q '^[0-9]\+.[0-9]\+-[0-9]\+$' ; then
+  echo "ERROR: invalid format of release string" >&2
+  exit 1
+fi
+
+# Set working directory to base directory of package:
+thisdir="$( dirname "$0" )"
+cd "$thisdir/.."
+
+# Define function to convert date formats:
+if date -v 1d >/dev/null 2>&1; then
+  # BSD date
+  DATEFMT() {
+    date -j -f '%Y-%m-%d %H:%M:%S %z' "$1" +%Y%m%d%H%M.%S
+  }
+else
+  # GNU date
+  DATEFMT() {
+    date -d "$1" +%Y%m%d%H%M.%S
+  }
+fi  
+
+# Check that generated files are up-to-date:
+if test -n "$( git status --porcelain )"; then
+  echo "WARNING: uncommitted changes in package" >&2
+fi
+
+for file in configure configure.ac tools/convert.m4 src/convert.c ; do
+  if test -n "$(git status --porcelain "$file")"; then
+    echo "ERROR: $file has uncommitted changes" >&2
+    exit 2
+  else
+    # Set timestamp on file to match last commit:
+    time="$(git log --pretty=format:%ad -n 1 --date=iso -- "$file")"
+    time="$(DATEFMT "$time")"
+    touch -m -t "$time" "$file"
+  fi
+done
+
+if [[ "configure.ac" -nt "configure" ]]; then
+  echo "ERROR: configure.ac is newer than configure" >&2
+  exit 2
+fi
+
+if [[ "tools/convert.m4" -nt "src/convert.c" ]]; then
+  echo "ERROR: tools/convert.m4 is newer than src/convert.c" >&2
+  exit 3
+fi
+
+# Check that NEWS contains a description of the release:
+if ! grep -q "$newver" NEWS; then
+  echo "WARNING: NEWS has no entry for release $newver" >&2
+fi
+
+# Get existing version string:
+oldver="$( grep 'Version: ' DESCRIPTION | awk '{ print $2 }' )"
+
+# Replace version string in all files (excluding hidden files).
+# In-place option of sed is not portable between GNU and BSD versions.
+find . -mindepth 1 -name '.*' -prune -o -type f ! -name NEWS -print | while read file; do
+    sed "s|$oldver|$newver|g" "$file" >"$file.sed"
+    if [[ -x "$file" ]]; then
+      # Preserve execute permissions:
+      chmod +x "$file.sed"
+    fi
+    mv "$file.sed" "$file"
+  done
+
+# Update release date in DESCRIPTION:
+newdate="$( date +%Y-%m-%d )"
+sed "s|Date:.*|Date: $newdate|" DESCRIPTION >DESCRIPTION.sed
+mv DESCRIPTION.sed DESCRIPTION
