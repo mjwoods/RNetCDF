a179e9304ac3bd8d8e0afe138342effeebe15731
Milton Woods <miltonjwoods@gmail.com>
2025-04-27 19:14:37 +1000

    Use new R API functions for environments (#159)

--- src/convert.c
+++ src/convert.c
@@ -6901,8 +6901,12 @@ R_nc_enum_factor (R_nc_buf *io)
   /* Create a hashed environment for value-index pairs.
      Members inherit PROTECTion from the env.
    */
+#if defined(HAVE_DECL_R_NEWENV) && (HAVE_DECL_R_NEWENV == 1)
+  env = PROTECT(R_NewEnv(R_BaseEnv, TRUE, 0));
+#else
   cmd = PROTECT(lang1 (install ("new.env")));
   env = PROTECT(eval (cmd, R_BaseEnv));
+#endif
 
   /* Read values and names of netcdf enum members.
      Store names as R factor levels.
@@ -6942,7 +6946,11 @@ R_nc_enum_factor (R_nc_buf *io)
   any_undef = 0;
   for (ifac=0, inval=io->cbuf; ifac<nfac; ifac++, inval+=size) {
     symbol = PROTECT(R_nc_char_symbol (inval, size, work));
+#if defined(HAVE_DECL_R_GETVAREX) && (HAVE_DECL_R_GETVAREX == 1)
+    index = R_getVarEx(symbol, env, FALSE, R_UnboundValue);
+#else
     index = findVar (symbol, env);
+#endif
     UNPROTECT(1);
     if (index == R_UnboundValue) {
       /* Convert undefined enum values to NA,
